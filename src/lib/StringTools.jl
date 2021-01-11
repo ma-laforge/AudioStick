e59803e1bdd8172067beb10791fd63620705c291
MA Laforge <ma.laforge.49@gmail.com>
2021-01-11 08:25:41 -0500

    Prepare for code re-structuring.

--- src/lib/StringTools.jl
+++
@@ -1,8 +0,0 @@
-module StringTools
-
-export parsestring
-
-parsestring{T<:Real}(::Type{T}, value::String) = parse(T, value)
-parsestring(::Type{String}, value::String) = String(value)
-
-end #StringTools
